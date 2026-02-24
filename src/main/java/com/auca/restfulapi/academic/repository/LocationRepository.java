1b60fc6098ea3e8e273c33242595ecc86391ac65
Pacifique16 <harerimanapacifique95@gmail.com>
2026-02-24 23:41:52 +0200

    Add repository methods for location queries by type and parent

--- src/main/java/com/auca/restfulapi/academic/repository/LocationRepository.java
+++ src/main/java/com/auca/restfulapi/academic/repository/LocationRepository.java
@@ -1,10 +1,12 @@
 package com.auca.restfulapi.academic.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.auca.restfulapi.academic.model.ELocationType;
 import com.auca.restfulapi.academic.model.Location;
 
 @Repository
@@ -12,4 +14,6 @@ public interface LocationRepository extends JpaRepository<Location,UUID > {
     
     Boolean existsByCode(String code);
     Location findByCode(String code);
+    List<Location> findByType(ELocationType type);
+    List<Location> findByParentId(UUID parentId);
 }
