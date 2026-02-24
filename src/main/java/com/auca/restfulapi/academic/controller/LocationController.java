fb5a036febde3b4ba8f73f33e72e0c7a64196e99
Pacifique16 <harerimanapacifique95@gmail.com>
2026-02-24 23:42:49 +0200

    Add endpoint to get location by ID

--- src/main/java/com/auca/restfulapi/academic/controller/LocationController.java
+++ src/main/java/com/auca/restfulapi/academic/controller/LocationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.auca.restfulapi.academic.model.Location;
 import com.auca.restfulapi.academic.service.LocationService;
@@ -29,6 +31,15 @@ public class LocationController {
     @GetMapping
     public ResponseEntity<List<Location>> getAllLocations(){
         return new ResponseEntity<>(locationService.getAllLocations(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getLocationById(@PathVariable UUID id){
+        Location location = locationService.getLocationById(id);
+        if(location != null){
+            return new ResponseEntity<>(location, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Location not found", HttpStatus.NOT_FOUND);
     }
 
     @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
