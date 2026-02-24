97cc7b9c1665c8f50e9508f37ac2333083af0d75
Pacifique16 <harerimanpacifique95@gmail.com>
2026-02-24 20:48:25 +0200

    Return 404 when no products found in price range

--- src/main/java/com/auca/restfulapi/ecommerce/controller/ProductController.java
+++ src/main/java/com/auca/restfulapi/ecommerce/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.auca.restfulapi.ecommerce.controller;
 
-import com.auca.restfulapi.ecommerce.model.Product;
-import com.auca.restfulapi.ecommerce.service.ProductService;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import com.auca.restfulapi.ecommerce.model.Product;
+import com.auca.restfulapi.ecommerce.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
@@ -46,9 +56,16 @@ public class ProductController {
         return ResponseEntity.ok(productService.searchProducts(keyword));
     }
 
+    
+    
     @GetMapping("/price-range")
-    public ResponseEntity<List<Product>> getProductsByPriceRange(@RequestParam Double min, @RequestParam Double max) {
-        return ResponseEntity.ok(productService.getProductsByPriceRange(min, max));
+    public ResponseEntity<?> getProductsByPriceRange(@RequestParam Double min, @RequestParam Double max) {
+        List<Product> products = productService.getProductsByPriceRange(min, max);
+        if (products.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "No products found in this price range"));
+        }
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("/in-stock")
